60bf3d92a22035ecc792ad032a90dc566630087d
nmbolanos <nathalybolanos243@gmail.com>
2026-06-02 10:37:26 -0500

    Proyecto InventarioPro - estructura inicial

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API Inventario funcionando");
+});
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en puerto 3000");
+});
